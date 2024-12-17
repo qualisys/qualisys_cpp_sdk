b73883c6b8b1d4985783da5a7fc73a5c7410814a
johannes.gotlen <johannes.gotlen@qualisys.se>
2024-12-17 11:56:03 +0100

    Added connect fallback version

--- RTProtocol.cpp
+++ RTProtocol.cpp
@@ -67,6 +67,7 @@ namespace
             // Valid versions added from high to low
             std::vector<RTVersion> versions {
                 {MAJOR_VERSION, MINOR_VERSION},
+                {1, 26},
                 {1, 25},
                 {1, 24},
                 {1, 23},
