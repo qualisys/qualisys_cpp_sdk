ae51cce1453cee1b8e020f4ef609a75e58745f02
johannes.gotlen <johannes.gotlen@qualisys.se>
2025-01-24 09:59:01 +0100

    Fixup GetCalibrationTest

--- Tests/Data/Calibration.h
+++ Tests/Data/Calibration.h
@@ -5,7 +5,7 @@ inline const char* Calibration = R"XMLDATA(
     <calibration calibrated="true" source="source.qca" created="Calibration carried out: 2019-09-17 16:00:43" qtm-version="2024.3 (build 14360)" type="regular" wandLength="601.100000" maximumFrames="1500" lengthOfCalibration="90" shortArmEnd="365.000000" longArmEnd="566.000000" longArmMiddle="200.000000">
         <results std-dev="1.959473" min-max-diff="41.064426"/>
         <cameras>
-            <camera active="1" calibrated="true" message="" point-count="100" avg-residual="0.01" serial="12345" model="Test Camera" viewrotation="180" video_resolution="1080p">
+            <camera active="1" calibrated="true" message="calibrated" point-count="100" avg-residual="0.01" serial="12345" model="Test Camera" viewrotation="180" video_resolution="1080p">
                 <fov_marker left="1" top="2" right="3" bottom="4"/>
                 <fov_marker_max left="5" top="6" right="7" bottom="8"/>
                 <fov_video left="9" top="10" right="11" bottom="12"/>
