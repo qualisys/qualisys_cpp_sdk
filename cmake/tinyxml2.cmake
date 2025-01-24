c4ee5b4b8cfa1b40664cb6e8b21251e708db8cc9
johannes.gotlen <johannes.gotlen@qualisys.se>
2025-01-24 13:06:26 +0100

    Use v10.0.0 of tinyxml2

--- cmake/tinyxml2.cmake
+++ cmake/tinyxml2.cmake
@@ -5,9 +5,11 @@ include(FetchContent)
 FetchContent_Declare(
     tinyxml2
     GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
-    GIT_TAG        master
+    GIT_TAG        321ea883b7190d4e85cae5512a12e5eaa8f8731f
 )
 
 FetchContent_MakeAvailable(tinyxml2)
+
+message(STATUS "Setting tinyxml2 include directory: ${tinyxml2_SOURCE_DIR}")
 
 include_directories(PUBLIC ${tinyxml2_SOURCE_DIR})
