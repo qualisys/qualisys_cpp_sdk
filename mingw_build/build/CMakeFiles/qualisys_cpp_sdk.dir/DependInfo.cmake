dda77f1b649708b1637d43f00b8314d19903e687
Oliver Glandberger <32294492+OliverGlandberger@users.noreply.github.com>
2025-02-06 17:31:08 +0100

    [Feature Branch] - Refactor Naming Deserializer (#98)
    * Started renaming variables in Tinyxml2Serializer.
    
    * Finished renaming.
    
    * Minor updates.
    
    * Remove the unnecessary comments, some minor changes.
    
    * In the middle of renaming many parameter and variable names. Code builds and tests pass. Saving progress.
    
    * Finished renaming in 'RTProtocol.h'.
    
    * Finished renaming parameters and variables in 'RTProtocol.cpp'.
    
    * Finished with 'Settings.h'.
    
    * Merged feature branch, fixed conflicts.
    
    * Additional minor changes.
    
    * Reverting Deserializer files.
    
    * Revert accidental change from x,y,z to x,x,x.
    
    * Removing 'e' from two local variables in 'SetCameraVideoSettingsTest'.
    
    * Reverted changes to 'Settings.h', 'RTProtocol.h', and test files. Also reverted changes that are not required right now in MarkupSerializer.cpp.
    
    * Refactoring Deserializer names, some small changes in RTProtocol.cpp and MarkupSerializer .h and .cpp.
    
    * Update some additional names.

---
+++ mingw_build/build/CMakeFiles/qualisys_cpp_sdk.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/qualisys_cpp_sdk/Markup.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/Markup.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/Markup.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/MarkupSerializer.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/MarkupSerializer.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/MarkupSerializer.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/Network.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/Network.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/Network.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/RTPacket.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/RTPacket.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/RTPacket.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/RTProtocol.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/RTProtocol.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/RTProtocol.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/Settings.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/Settings.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/Settings.cpp.obj.d"
+  "C:/qualisys_cpp_sdk/Tinyxml2Serializer.cpp" "CMakeFiles/qualisys_cpp_sdk.dir/Tinyxml2Serializer.cpp.obj" "gcc" "CMakeFiles/qualisys_cpp_sdk.dir/Tinyxml2Serializer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mingw_build/build/_deps/tinyxml2-subbuild/CMakeFiles/tinyxml2-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for tinyxml2-populate.
+# This may be replaced when dependencies are built.
