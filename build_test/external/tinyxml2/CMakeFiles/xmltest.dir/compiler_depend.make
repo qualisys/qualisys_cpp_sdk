3f6f920dda5b0eb747c253b0bbac8fcf44bc4c6c
Oliver Glandberger <oliver.glandberger@qualisys.se>
2025-02-11 16:36:19 +0100

    Remove 'build_test' folder.

--- build_test/external/tinyxml2/CMakeFiles/xmltest.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for xmltest.
-# This may be replaced when dependencies are built.
--- build_test/external/tinyxml2/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/qualisys_cpp_sdk/external/tinyxml2
-# Build directory: C:/qualisys_cpp_sdk/build_test/external/tinyxml2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "C:/qualisys_cpp_sdk/build_test/external/tinyxml2/xmltest.exe")
-set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "C:/qualisys_cpp_sdk/external/tinyxml2" _BACKTRACE_TRIPLES "C:/qualisys_cpp_sdk/external/tinyxml2/CMakeLists.txt;49;add_test;C:/qualisys_cpp_sdk/external/tinyxml2/CMakeLists.txt;0;")
