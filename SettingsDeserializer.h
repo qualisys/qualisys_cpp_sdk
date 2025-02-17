3820604b362dfee07bf61f745cebcf311b1b9830
johannes.gotlen <johannes.gotlen@qualisys.se>
2025-02-17 11:11:34 +0100

    Added missing formal parameter name in  Deserialize6DOFSettings

--- SettingsDeserializer.h
+++ SettingsDeserializer.h
@@ -17,7 +17,7 @@ namespace qualisys_cpp_sdk
         bool DeserializeAnalogSettings(std::vector<SAnalogDevice>& analogDeviceSettings, bool& dataAvailable);
         bool DeserializeForceSettings(SSettingsForce& forceSettings, bool& dataAvailable);
         bool DeserializeImageSettings(std::vector<SImageCamera>& imageSettings, bool& dataAvailable);
-        bool Deserialize6DOFSettings(std::vector<SSettings6DOFBody>& settings6Dof, SSettingsGeneral&,
+        bool Deserialize6DOFSettings(std::vector<SSettings6DOFBody>& settings6Dof, SSettingsGeneral& generalSettings,
                                      bool& dataAvailable);
         bool DeserializeGazeVectorSettings(std::vector<SGazeVector>& gazeVectorSettings, bool& dataAvailable);
         bool DeserializeEyeTrackerSettings(std::vector<SEyeTracker>& eyeTrackerSettings, bool& dataAvailable);
