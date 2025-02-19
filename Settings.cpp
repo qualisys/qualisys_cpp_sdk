adb49f46974b8dd01330d484b83f49882447759e
johannes.gotlen <johannes.gotlen@qualisys.se>
2025-02-19 17:41:13 +0100

    Fixes issue when compiling with whole program optimization

--- Settings.cpp
+++ Settings.cpp
@@ -1,5 +1,18 @@
 #include "Settings.h"
 
+namespace
+{
+    constexpr auto DEGREES_OF_FREEDOM =
+    {
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::RotationX, "RotationX"),
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::RotationY, "RotationY"),
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::RotationZ, "RotationZ"),
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::TranslationX, "TranslationX"),
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::TranslationY, "TranslationY"),
+        std::make_pair(qualisys_cpp_sdk::EDegreeOfFreedom::TranslationZ, "TranslationZ")
+    };
+}
+
 const char* qualisys_cpp_sdk::SkeletonDofToStringSettings(EDegreeOfFreedom dof)
 {
     auto it = std::find_if(DEGREES_OF_FREEDOM.begin(), DEGREES_OF_FREEDOM.end(), [&](const auto& DEGREE_OF_FREEDOM) { return (DEGREE_OF_FREEDOM.first == dof); });
