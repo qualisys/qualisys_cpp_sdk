ac4e25fae8aded3ca8e404751c5ea5ce1d71256b
qjgn <45095781+qjgn@users.noreply.github.com>
2025-02-21 13:51:29 +0100

    Tinyxml2 fix leaking consts (#122)
    * Fixed leaking consts from tinyxml2

--- External/tinyxml2/tinyxml2.cpp
+++ External/tinyxml2/tinyxml2.cpp
@@ -119,6 +119,8 @@ distribution.
 	#define TIXML_FTELL ftell
 #endif
 
+namespace
+{
 
 static const char LINE_FEED				= static_cast<char>(0x0a);			// all line endings are normalized to LF
 static const char LF = LINE_FEED;
@@ -134,6 +136,8 @@ static const char DOUBLE_QUOTE			= '\"';
 static const unsigned char TIXML_UTF_LEAD_0 = 0xefU;
 static const unsigned char TIXML_UTF_LEAD_1 = 0xbbU;
 static const unsigned char TIXML_UTF_LEAD_2 = 0xbfU;
+
+}
 
 namespace tinyxml2
 {
